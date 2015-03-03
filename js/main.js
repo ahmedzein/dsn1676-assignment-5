f064e0ffb1898e25bb77d3dc8e486627a1cb1100
ahmedzein <elze0010@algonquinlive.com>
2015-03-02 23:46:58 -0500

    classes and js
    changed the names of classes
    rearranged the js code
    changed from an h1 to li
    added delete button

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 var $form = $('form');
 var $input = $('#list-item');
 var $theTask = $('.task');
-var $currentTask = $('#task');
+
 
 $form.on('submit', function(eventObject) {
     eventObject.preventDefault();
     
-    var $theH1 = $('<h1>');
-    $theH1.html($input.val());
+    var $theLi = $('<li>').html($input.val());
+
+    var $done =$('<button class="finished">X</button>')
     
-    $theTask.append($theH1);
-    
-    $input.val('');
+$done.on('click', function () {
+    $theLi.remove('li');
 });
 
-$theTask.on('click', 'h1' , function () {
-   $(this).addClass('js-highlight');
+$theTask.on('click', 'li' , function () {
+   $(this).addClass('js-done');
 });
+    
+    $theLi.append($done);
+    $theTask.append($theLi);
+    $input.val('');
+    
+ });
